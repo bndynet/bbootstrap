9cf5cf46106af66adfca0a52990428c07777cceb
Bendy Zhang <zb@bndy.net>
2018-07-07 12:19:21 +0800

    loading styles

--- README.md
+++ README.md
@@ -17,6 +17,7 @@ A set which includes some useful components.
 - [bootstrap](http://getbootstrap.com) 4.1.1
 - [font-awesome](https://fontawesome.com/) 4.7.0
 - [animate.css](https://daneden.github.io/animate.css/) 3.6.1
+- [jslib](https://github.com/bndynet/jslib) v1.0.55
 
 Note: Do not include above components in your project. This project has included them.
 
@@ -29,6 +30,18 @@ Note: Do not include above components in your project. This project has included
 ```
 
 ## Changelog
+
+
+### v1.0.2
+
+- Add styles about overflow
+- Add styles about status and loading
+- Add styles about link and link list
+- Add tooltip via JavaScript
+    ```js
+    $('#id').btooltip('title', 'placement');    // show tooltip, placement is optional
+    $('#id').btooltip(false);                   // close tooltip
+    ```
 
 ### v1.0.1
 
--- src/js/bbootstrap.js
+++ src/js/bbootstrap.js
@@ -1,8 +1,71 @@
 //=require jquery/dist/jquery.js
-//=require popper.js/dist/umd/popper.min.js
+//=require popper.js/dist/umd/popper.js
 //=require bootstrap/dist/js/bootstrap.js
+//=require @bndynet/jslib/dist/jslib.min.js
+
+$.fn.extend({
+    loading: function(destoryOrloadingStyle, theme) {
+        if (destoryOrloadingStyle === false) {
+            $(this).cover(false);
+            return;
+        }
+
+        loadingStyle = destoryOrloadingStyle || 'bounce';
+        theme = theme ? 'loading-' + theme : '';
+        var htmlLoading = '';
+        switch((loadingStyle||'').toLowerCase()) {
+            case 'bounce':
+                htmlLoading = '<div class="loading ' + theme + ' loading-bounce"><div class="child1"></div><div class="child2"></div><div class="child3"></div></div>';
+                break;
+
+            case 'bounce-rectangle':
+                htmlLoading = '<div class="loading ' + theme + ' loading-bounce-rectangle">' +
+                    '<div class="child1"></div>' +
+                    '<div class="child2"></div>' +
+                    '<div class="child3"></div>' +
+                    '<div class="child4"></div>' +
+                    '<div class="child5"></div>' +
+                    '</div>';
+                break;
+
+            case 'circle':
+                htmlLoading = '<div class="loading ' + theme + ' loading-circle">' + 
+                    '<div class="child1 loading-circle-child"></div>' + 
+                    '<div class="child2 loading-circle-child"></div>' + 
+                    '<div class="child3 loading-circle-child"></div>' + 
+                    '<div class="child4 loading-circle-child"></div>' + 
+                    '<div class="child5 loading-circle-child"></div>' + 
+                    '<div class="child6 loading-circle-child"></div>' + 
+                    '<div class="child7 loading-circle-child"></div>' + 
+                    '<div class="child8 loading-circle-child"></div>' + 
+                    '<div class="child9 loading-circle-child"></div>' + 
+                    '<div class="child10 loading-circle-child"></div>' + 
+                    '<div class="child11 loading-circle-child"></div>' + 
+                    '<div class="child12 loading-circle-child"></div>' + 
+                    '</div>';
+                break;
+        }
+        htmlLoading = htmlLoading || 'No `' + loadingStyle + '` style defined.';
+        $(this).cover(htmlLoading);
+    },
+
+    btooltip: function(destoryOrTitle, position) {
+        if (destoryOrTitle == false) {
+            $(this).tooltip('dispose');
+            return;
+        }
+        $(this).tooltip({
+            placement: position || 'top',
+            title: destoryOrTitle,
+            trigger: 'manual',
+        });
+        $(this).tooltip('show');
+    },
+});
+
 
 $(function() {
+    // file input
     $('body').on('change', '.custom-file-input', function() {
         var eleFile = $(this)[0];
         var eleLabel = $(eleFile).next();
@@ -21,4 +84,5 @@ $(function() {
             eleLabel.html(placeholder);
         }
     });
+
 });
